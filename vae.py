a6457049029d77dfddbe36044068c4cf1a5a8fb1
Bauke <bauke.brenninkmeijer@gmail.com>
2019-08-03 20:56:13 +0200

    Finish notebook. Update script but still WIP

--- vae.py
+++ vae.py
@@ -7,83 +7,64 @@ from torch.nn import functional as F
 from torchvision import datasets, transforms
 from torchvision.utils import save_image
 import numpy as np
-
-
-# parser = argparse.ArgumentParser(description='VAE MNIST Example')
-# parser.add_argument('--batch-size', type=int, default=128, metavar='N',
-#                     help='input batch size for training (default: 128)')
-# parser.add_argument('--epochs', type=int, default=10, metavar='N',
-#                     help='number of epochs to train (default: 10)')
-# parser.add_argument('--no-cuda', action='store_true', default=False,
-#                     help='enables CUDA training')
-# parser.add_argument('--seed', type=int, default=1, metavar='S',
-#                     help='random seed (default: 1)')
-# parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-#                     help='how many batches to wait before logging training status')
-# args = parser.parse_args()
-# args.cuda = not args.no_cuda and torch.cuda.is_available()
-
-# torch.manual_seed(args.seed)
-#
-# device = torch.device("cuda" if args.cuda else "cpu")
-
-# kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
-# train_loader = torch.utils.data.DataLoader(
-#     datasets.MNIST('../data', train=True, download=True,
-#                    transform=transforms.ToTensor()),
-#     batch_size=args.batch_size, shuffle=True, **kwargs)
-# test_loader = torch.utils.data.DataLoader(
-#     datasets.MNIST('../data', train=False, transform=transforms.ToTensor()),
-#     batch_size=args.batch_size, shuffle=True, **kwargs)
-
+from fastai.vision import *
 
 class VAE(nn.Module):
-    def __init__(self, size_in, size_emb):
+    def __init__(self, size_in, size_emb, dec_siz=[400], enc_siz=[400], channels=3):
         super().__init__()
-        print(size_in, size_emb)
-        self.fc1 = nn.Linear(size_in, 400)
-        self.fc21 = nn.Linear(400, size_emb)
-        self.fc22 = nn.Linear(400, size_emb)
-        self.fc3 = nn.Linear(size_emb, 400)
-        self.fc4 = nn.Linear(400, size_in)
-        torch.nn.init.xavier_uniform_(self.fc1.weight)
-        torch.nn.init.xavier_uniform_(self.fc21.weight)
-        torch.nn.init.xavier_uniform_(self.fc22.weight)
-        torch.nn.init.xavier_uniform_(self.fc3.weight)
-        torch.nn.init.xavier_uniform_(self.fc4.weight)
+        self.enc = []
+        self.dec = []
+        self.channels = channels
+        self.size_in = size_in
+        self.size_in_sqr = size_in**2
+        enc_siz = [self.size_in_sqr*channels] + enc_siz
+        dec_siz = [size_emb] + dec_siz
 
-        self.bn1 = nn.BatchNorm1d(400)
-        self.bn2 = nn.BatchNorm1d(size_emb)
-        self.bn3 = nn.BatchNorm1d(size_emb)
+        for i, layer in enumerate(enc_siz):
+            if i + 1 == len(enc_siz): break
+            self.enc += [
+                nn.Linear(enc_siz[i], enc_siz[i+1], bias=False),
+                nn.ReLU(),
+                nn.BatchNorm1d(enc_siz[i+1])
+            ]
+
+        for i, layer in enumerate(dec_siz):
+            if i + 1 == len(dec_siz): break
+            self.dec += [
+                nn.Linear(dec_siz[i], dec_siz[i + 1], bias=False),
+                nn.ReLU(),
+                nn.BatchNorm1d(dec_siz[i + 1])
+            ]
+
+        self.encoder = nn.Sequential(*self.enc)
+        self.decoder = nn.Sequential(*self.dec)
+
+        self.fc_mu = nn.Linear(enc_siz[-1], size_emb)
+        self.fc_std = nn.Linear(enc_siz[-1], size_emb)
+        self.out = nn.Linear(dec_siz[-1], self.size_in_sqr*self.channels)
+
+        self.bn_mu = nn.BatchNorm1d(size_emb)
+        self.bn_std = nn.BatchNorm1d(size_emb)
 
         self.mu = None
         self.logvar = None
 
     def encode(self, x):
-        x = x[:, 0, ...]
-        x = x.view(-1, 784)
-        # print(x)
-        h1 = self.bn1(F.relu(self.fc1(x)))
-        # print(h1)
-        return self.bn2(self.fc21(h1)), self.bn2(self.fc22(h1))
+        x = self.encoder(x.view(-1, self.size_in_sqr*self.channels))
+        return self.bn_mu(self.fc_mu(x)), self.bn_std(self.fc_std(x))
 
     @staticmethod
     def reparameterize(mu, logvar):
         std = torch.exp(0.5*logvar)
         eps = torch.randn_like(std)
-        return mu + eps*std
+        return mu + eps * std
 
     def decode(self, z):
-        h3 = F.relu(self.fc3(z))
-        b = z
-        d = h3
-        # print(np.sum(np.isnan(b.detach().numpy())))
-        # print(np.sum(np.isnan(d.detach().numpy())))
-        # print(b, d)
-        return torch.cat([torch.sigmoid(self.fc4(h3))]*3).view(-1, 3, 28, 28)
+        x = self.decoder(z)
+        return torch.sigmoid(self.out(x)).view(-1, self.channels, self.size_in, self.size_in)
 
     def forward(self, x):
-        mu, logvar = self.encode(x.view(-1, 3, 784))
+        mu, logvar = self.encode(x)
         z = self.reparameterize(mu, logvar)
         self.mu = mu
         self.logvar = logvar
@@ -91,23 +72,9 @@ class VAE(nn.Module):
 
     # Reconstruction + KL divergence losses summed over all elements and batch
     def vae_loss_function(self, recon_x, x):
-        # print(recon_x, x)
-        # print(recon_x.shape, x.shape)
-        # print(np.sum(np.isnan(recon_x.numpy())), np.sum(np.isnan(x.numpy())))
-        BCE = F.binary_cross_entropy(recon_x.view(-1, 3, 28, 28), x.view(-1, 3, 28, 28), reduction='sum')
-
-        # see Appendix B from VAE paper:
-        # Kingma and Welling. Auto-Encoding Variational Bayes. ICLR, 2014
-        # https://arxiv.org/abs/1312.6114
-        # 0.5 * sum(1 + log(sigma^2) - mu^2 - sigma^2)
+        BCE = F.binary_cross_entropy(recon_x, x, reduction='sum')
         KLD = -0.5 * torch.sum(1 + self.logvar - self.mu.pow(2) - self.logvar.exp())
         return BCE + KLD
-
-
-# model = VAE().to(device)
-# optimizer = optim.Adam(model.parameters(), lr=1e-3)
-
-
 
 
 def train(epoch):
@@ -149,12 +116,46 @@ def test(epoch):
     test_loss /= len(test_loader.dataset)
     print('====> Test set loss: {:.4f}'.format(test_loss))
 
-# if __name__ == "__main__":
-#     for epoch in range(1, args.epochs + 1):
-#         train(epoch)
-#         test(epoch)
-#         with torch.no_grad():
-#             sample = torch.randn(64, 20).to(device)
-#             sample = model.decode(sample).cpu()
-#             save_image(sample.view(64, 1, 28, 28),
-#                        'results/sample_' + str(epoch) + '.png')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='VAE MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=128, metavar='N',
+                        help='input batch size for training (default: 128)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--no-cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--emb-size', type=int, default=10, help='size of embedding (default 10)')
+    args = parser.parse_args()
+    args.cuda = not args.no_cuda and torch.cuda.is_available()
+
+    torch.manual_seed(42)
+
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    mnist = untar_data(URLs.MNIST_TINY)
+    tfms = get_transforms(do_flip=False)
+
+    data = (ImageImageList.from_folder(mnist/'train')
+            .split_by_rand_pct(0.1, seed=42)
+            .label_from_func(lambda x: x)
+            .transform(tfms)
+            .databunch(num_workers=0, bs=16)
+            .normalize(do_y=True))
+
+    image_size = data.one_batch()[0].shape[-1]
+    vae = VAE(image_size, args.emb_size)
+    my_learner = Learner(data,
+                         vae,
+                         opt_func=torch.optim.Adam,
+                         loss_func=vae.vae_loss_function)
+    my_learner.fit(args.epochs, lr=1e-2)
+    my_learner.show_results(rows=4)
+
+    # for epoch in range(1, args.epochs + 1):
+    #     train(epoch)
+    #     test(epoch)
+    #     with torch.no_grad():
+    #         sample = torch.randn(64, 20).to(device)
+    #         sample = model.decode(sample).cpu()
+    #         save_image(sample.view(64, 1, 28, 28),
+    #                    'results/sample_' + str(epoch) + '.png')
